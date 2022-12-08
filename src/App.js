4176826ae578dcda9ffd686a530b644a793cf279
Sunil <snilavarath@gmail.com>
2022-12-08 13:23:09 -0500

    new navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import NavBar from "./Components/NavBar";
-import Home from "./Components/Home";
-import Footer from "./Components/Footer";
-import Resume from "./Components/Resume";
+import "./App.scss";
+// import NavBar from "./Components/Navbar/NavBar";
+import { Navbar } from "./Components";
+// import Home from "./Components/Home";
+// import Footer from "./Components/Footer";
+// import Resume from "./Components/Resume";
 import VideoCards from "./Components/VideoCards";
-import Skills from "./Components/Skills";
+// import Skills from "./Components/Skills";
 import SkillIcons from "./Components/SkillIcons";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Home,
+  Resume,
+  Projects,
+} from "./container";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div className="app">
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/resume" element={<Resume />} />
         <Route exact path="/projects" element={<VideoCards />} />
         <Route exact path="/skills" element={<SkillIcons />} />
       </Routes>
+      <Header />
       <Footer />
     </div>
   );
