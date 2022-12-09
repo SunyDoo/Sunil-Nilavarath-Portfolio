3d052301eeb1f452ae66f775dd43085edb307759
Sunil <snilavarath@gmail.com>
2022-12-09 18:37:47 -0500

    projects section start

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import youtube from "../../videos";
 
-import './Projects.scss'
+import { AppWrap } from "../../wrapper";
+
+import "./Projects.scss";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <>
+      <h4 className="head-text">
+        My
+        <span> Portfolio </span>
+        of <span> Works </span>
+      </h4>
+      <center>
+        <div className="app__work-filter-item">
+          {youtube.videos.map((video) => (
+            <div className="app__works" style={{ padding: "2%" }}>
+              <center>
+                <iframe
+                  width="100%"
+                  height="500"
+                  src={video.link}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  title="Embedded youtube"
+                />
+              </center>
+              <div className="app__works">
+                <h5 className="card title">{video.name}</h5>
+                <ul className="card-text">
+                  {video.bullets.map((bullet) => (
+                    <li id={video.id}>{bullet}</li>
+                  ))}
+                </ul>
+                <motion.div>
+                  <p className="card-text">
+                    <a className="text-blue" href={video.github}>
+                      <AiFillGithub />
+                      Github
+                    </a>
+                  </p>
+                  {video.site? <p className="card-text">
+                    <a className="text-blue" href={video.github}>
+                      <AiFillGithub />
+                      {video.site}
+                    </a>
+                  <AiFillEye />
+                  </p> : null}
+                  
+                </motion.div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </center>
+    </>
+  );
+};
 
-export default Projects
+export default AppWrap(Projects, "projects");
--- src/videos.js
+++ src/videos.js
@@ -1,47 +1,50 @@
-const youtube ={
-    "videos": [
-      {
-        "id": 1,
-        "name": "RUMC Patient Portal",
-        "link": "https://www.youtube.com/embed/s11rA9Hou4s",
-        "github": "https://github.com/SunyDoo/flatiron-capstone-rumc-patient-portal",
-        "bullets": 
-          [
-            `Implemented authentication/authorization, including password protection, to allow users to log in to the site with a secure password and stay logged in via user ID in the session hash`, `Following REST conventions, ran full CRUD on appointments being created`, `Utilized the Mapbox GL API to display an interactive map of all 12 doctor’s offices affiliated with the hospital`
-          ]
-        
-      },
-      {
-        "id": 2,
-        "name": "Sunil's BlogSpot",
-        "link": "https://www.youtube.com/embed/W7XaT3JDzz0",
-        "github": "https://github.com/SunyDoo/phase-4-final-project",
-        "bullets": 
-          [
-            `Implemented password protection and authentication along with validations and error handling when signing up for an account`, `Used a Rails API backend running full CRUD with a React frontend to create a fully functioning web application `, `Implemented React Router to run three different client-side routes using a nav bar to allow users to navigate between routes`
-          ]
-      },
-      {
-        "id": 3,
-        "name": "Sunil's Movie Reviews",
-        "link": "https://www.youtube.com/embed/0ruEEsFAD3c",
-        "github": "https://github.com/SunyDoo/phase-3-frontend",
-        "bullets": 
-          [
-            `Used a Sinatra-based backend running full CRUD with a React frontend to create a fully functioning web application `, `Implemented React Router to run four client-side routes using a nav bar to allow users to navigate between routes`
-          ]
-      },
-      {
-        "id": 4,
-        "name": "Sunil's Gym",
-        "link": "https://www.youtube.com/embed/hErkcaaHIC8",
-        "github": "https://github.com/SunyDoo/Phase2-Final-Project",
-        "bullets": 
-          [
-            `Simple gym application where users can select exercises to add to their workout routine for the day`, `Users can sort through available exercises based on body part`
-          ]
-      },
-      
-    ]
-  }
-  export default youtube;
+const youtube = {
+  videos: [
+    {
+      id: 1,
+      name: "RUMC Patient Portal",
+      link: "https://www.youtube.com/embed/s11rA9Hou4s",
+      github:
+        "https://github.com/SunyDoo/flatiron-capstone-rumc-patient-portal",
+      site: "https://rumc-patient-portal.herokuapp.com/",
+      bullets: [
+        `Implemented authentication/authorization, including password protection, to allow users to log in to the site with a secure password and stay logged in via user ID in the session hash`,
+        `Following REST conventions, ran full CRUD on appointments being created`,
+        `Utilized the Mapbox GL API to display an interactive map of all 12 doctor’s offices affiliated with the hospital`,
+      ],
+    },
+    {
+      id: 2,
+      name: "Sunil's BlogSpot",
+      link: "https://www.youtube.com/embed/W7XaT3JDzz0",
+      github: "https://github.com/SunyDoo/phase-4-final-project",
+      site: "https://sunils-blogspot.herokuapp.com/",
+      bullets: [
+        `Implemented password protection and authentication along with validations and error handling when signing up for an account`,
+        `Used a Rails API backend running full CRUD with a React frontend to create a fully functioning web application `,
+        `Implemented React Router to run three different client-side routes using a nav bar to allow users to navigate between routes`,
+      ],
+    },
+    {
+      id: 3,
+      name: "Sunil's Movie Reviews",
+      link: "https://www.youtube.com/embed/0ruEEsFAD3c",
+      github: "https://github.com/SunyDoo/phase-3-frontend",
+      bullets: [
+        `Used a Sinatra-based backend running full CRUD with a React frontend to create a fully functioning web application `,
+        `Implemented React Router to run four client-side routes using a nav bar to allow users to navigate between routes`,
+      ],
+    },
+    {
+      id: 4,
+      name: "Sunil's Gym",
+      link: "https://www.youtube.com/embed/hErkcaaHIC8",
+      github: "https://github.com/SunyDoo/Phase2-Final-Project",
+      bullets: [
+        `Simple gym application where users can select exercises to add to their workout routine for the day`,
+        `Users can sort through available exercises based on body part`,
+      ],
+    },
+  ],
+};
+export default youtube;
